107d0bd4b4c2359dcdde6bd3224b9585e0fef5c9
Mathieu <mathieu.rita@polytechnique.edu>
2018-03-13 08:28:31 +0100

    V2

--- analyse.py
+++ analyse.py
@@ -1,14 +1,15 @@
 import numpy as np
-import sys,os 
+import sys,os
 sys.path.append("/Users/MathieuRita/Desktop/PSC-info/PSC-neurobiologie/imgca-master")
 import imgca as ca
 import matplotlib.pyplot as plt
 import scipy as sc
 from scipy.ndimage.filters import gaussian_filter
-import scipy.integrate as integrate
-import scipy.special as special
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+from scipy.stats import pearsonr
 
 data,neuropil, conds, stim, dt, expname = ca.importRaw("/Users/MathieuRita/Desktop/PSC-info/160428_cage1_mouse2_d220")
+# data,neuropil, conds, stim, dt, expname = ca.importRaw("/Users/MathieuRita/Desktop/PSC-info/Data/180117_mouse1")
 
 nexp, ncell, nstim, nt = data.shape
 
@@ -19,9 +20,10 @@ rmcells2 = np.where(data[0].min(1).min(1)<-1)[0]
 data = np.delete(data, rmcells2,1)
 
 data=ca.format5D(data,"/Users/MathieuRita/Desktop/PSC-info/160428_cage1_mouse2_d220", conds, dt, method="minimal")
+# data=ca.format5D(data,"/Users/MathieuRita/Desktop/PSC-info/Data/180117_mouse1", conds, dt, method="minimal")
 
 #data=[num exp, num cell, num son, repetition, temps]
-  
+
 data=data-np.mean(data[:,:,:,:,:15], axis=4, keepdims=True)
 
 data=ca.deconvolve(data, dt, tau=2)
@@ -30,34 +32,6 @@ data=gaussian_filter(data,(0,0,0,0,2))
 
 data=data[:,:,:,:,5:-5]
 # permet de s'affranchir des effets de bords ou le filtrage gaussien fait potentiellement de la merde
-
-def normalize(v):
-    norm=np.linalg.norm(v, ord=2)
-    if norm==0:
-        norm=np.finfo(v.dtype).eps
-    moy=np.mean(v)
-    return (v-moy)/norm
-
-
-#def recouvrement(tau, s1, s2) :
-#    product = [s1[i]*s2[i+tau] for i in range(0, s2.shape() - tau - 1)]
-#    np.trapz(product)
-
-#def dephasage(s1,s2) :
-#    for tau in range(0, s1.shape()//4):
-#        L+=[recouvrement(tau,s1,s2)]
-#    return np.amin(L)
-
-#def deph(s1,s2) :
-#    max1=np.amax(s1)
-#    max2=np.amax(s2)
-#    i1=0
-#    i2=0
-#    while(s1[i1]!=max1) :
-#        i1+=1
-#    while(s2[i2]!=max2) :
-#        i2+=1
-#    return(i2-i1)
 
 
 def matcoress(son, temps1, temps2) :
@@ -68,21 +42,80 @@ def matcoress(son, temps1, temps2) :
     M=np.corrcoef(V1,V2)
     return(np.mean(M))
 
-#len=len(data[0,0,0,0,:])
-son=20
-#matcorr=np.zeros((len,len))
-#for i in range(0,len):
-#    for j in range(0,len) :
-#        matcorr[i,j]=matcoress(son,i,j)
-#
-#
-#plt.pcolor(matcorr)
-#plt.show()
+def matcortps(son) :
+    L=len(data[0,0,0,0,:])
+    matcorr=np.zeros((L,L))
+    for i in range(0,L):
+       for j in range(0,L) :
+           matcorr[i,j]=matcoress(son,i,j)
+    return matcorr
 
-X=normalize(data[0,8,son,1,:])
-Y=normalize(data[0,8,son,5,:])
-Z=np.linspace(-0.3,0.3)
-plt.scatter(X,Y,s=50)
-plt.plot(Z,Z)
-plt.show()
+def showmatcortps(son) :
+    plt.pcolor(matcortps(son))
+    plt.show()
 
+#Programme pour clustering :  - determiner une metrique agreable - faire un dendogramme cf internet
+
+#Pb on perd juste la donnee du num de la cellule
+
+def datared(data, son) :
+    nexp,ncell,nson,ness,nt=data.shape
+    seuil=0.04
+    selec=ness//5
+    datared=np.empty([0,nt])
+    for cell in range(0,ncell) :
+        cpt=0
+        tps=np.empty([0,nt])
+        for ess in range(0,ness) :
+            max=np.amax(data[0,cell,son,ess,:])
+            if float(max)>seuil :
+                cpt+=1
+                tps=np.append(tps,[data[0,cell,son,ess,:]], axis=0)
+        if cpt>selec :
+            datared=np.append(datared,[np.mean(tps,axis=0)], axis=0)
+    print(datared.shape)
+    return(datared)
+
+def correlationcell(data,son) :
+    M=np.corrcoef(datared(data,son))
+    print(M.shape)
+    plt.pcolor(M)
+    plt.show()
+
+#correlationcell(data,8)
+
+def clustering(data,son) :
+    #Matrice des liens
+    Z = linkage(datared(data,son),method='ward',metric='euclidean')
+
+    #Affichage du dendogramme
+    plt.title("CAH")
+    dendrogram(Z,orientation='left',color_threshold=0.6)
+    plt.show()
+    groupes_cah = fcluster(Z,t=0.6,criterion='distance')
+    print(groupes_cah.shape,groupes_cah)
+
+def clustering2(data,son) :
+    nt=51
+    datan=datared(data,son)
+    Z = linkage(datan,method='ward',metric='euclidean')
+    groupes_cah = fcluster(Z,t=0.6,criterion='distance')
+    L=len(groupes_cah)
+    nb=int(np.amax(groupes_cah))
+    dataord=np.empty([0,nt])
+    tps=np.empty([nb,0,nt])
+    for i in range(L) :
+        ind_clus=groupes_cah[i]
+        tps=np.append(tps,[datan[i,:]], axis=1)
+    dim1,dim2,dim3=tps[:,:,:].shape
+    print(dim1,dim2,dim3)
+    for k in range(dim1):
+        for j in range(dim2):
+            dataord=np.append(dataord,[tps[k,j,:]], axis=0)
+    print(dataord.shape)
+
+
+
+#idee est de recuperer lordre avec lequel le dendogramme classe et apres faire la mat de correlation dans cette ordre
+
+clustering2(data,4)
